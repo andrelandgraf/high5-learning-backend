582306ddd2ce826bc4e3dadf9423a1f778c05702
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-05-24 18:07:41 +0200

    Switched default port to 3010. If you are running the backend on another port you may have set up a .env File with a different port.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //Configuration variables
-const port      = process.env.PORT        || '3000';
+const port      = process.env.PORT        || '3010';
 const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost:27017/high-five-db';
 const JwtSecret = process.env.JWT_SECRET  ||'very high five e learning';
 
