1b35d97a77833c07698e54ed2d94c1ec2168b300
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-05-28 15:38:10 +0200

    Deleted deprecated ExerciseSchema Implemented SubmissionSchema

--- src/models/homework.js
+++ src/models/homework.js
@@ -4,18 +4,6 @@ const mongoose = require('mongoose');
 
 //definition of the homework schema
 
-//ExerciseSchema is a subdocument for the HomeworkSchema
-const ExerciseSchema = new mongoose.Schema({
-    question: {
-        type: String,
-        required: true
-    },
-    answers: {
-        type: [String],
-        required: true
-    }
-})
-
 //homework Schema with title of homework, corresponding exercises and assignedClass
 const HomeworkSchema = new mongoose.Schema({
     title: {
---
+++ src/models/submission.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+//definition of the submission schema
+
+//submission Schema with an array of indexes of the picked answer of each exercise of an homework and the assigned Homework
+const SubmissionSchema = new mongoose.Schema({
+    exercises:  {
+        type: [Number],
+        enum: [0, 1, 2, 3],
+        required: true
+    },
+    homework: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Homework',
+        required: true
+    },
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+},{ collection: 'submission' });
+
+//Setting configurable options to HomeworkSchema
+SubmissionSchema.set('timestamps', true);
+
+
+//Export of the homework model
+module.exports = mongoose.model('Submission', SubmissionSchema);
