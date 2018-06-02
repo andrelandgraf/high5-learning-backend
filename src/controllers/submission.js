c0f32e106ed80d4d7c0d045f9803219ffc7fa866
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-06-02 15:48:12 +0200

    Ref #21

--- src/controllers/submission.js
+++ src/controllers/submission.js
@@ -8,20 +8,130 @@ const UserModel = require('../models/user');
 const HomeworkModel = require('../models/homework');
 const SubmissionModel = require('../models/submission');
 
-const findByHomework = (req, res) => {
+// this will return the statistic
+const getStatisticsForHomework = (req, res) => {
     if (!Object.prototype.hasOwnProperty.call(req.params, 'id'))
         return res.status(400).json({
             error: 'Bad Request',
             message: 'The request body must contain a id property for the homework'
         });
     const homeworkId = req.params.id;
-    SubmissionModel.find({homework: homeworkId})
-        .then(submission => {
-            res.status(200).json(submission);
-        }).catch((error) => {
-        res.status(500).json(error);
-    });
+    getHomework(homeworkId).exec().then((homework) => mapReduceStatistic(req, res, homework))
+        .catch(error => {
+            console.log(error);
+            res.status(404).json({error: "Object not found"});
+        });
+
 };
+
+function getHomework(homeworkId) {
+    return HomeworkModel.findById(homeworkId);
+}
+
+// See: http://mongoosejs.com/docs/api.html#mongoose_Mongoose-Model
+function mapReduceStatistic(req, res, homework) {
+    let mapReduceObject = {};
+    mapReduceObject.map = function () {
+        // each submission contains the exercises array which stores the picked answer for each exercise of the student
+        this.exercises.forEach(function (pickedAnswer, i) {
+            // key is index of the exercise in exercise array
+            // value is map of:
+            emit(i, pickedAnswer);
+        });
+    };
+    mapReduceObject.reduce = function (keyExerciseIndex, mapArray) {
+        let pickedAnswers = [];
+        pickedAnswers[0] = 0;
+        pickedAnswers[1] = 0;
+        pickedAnswers[2] = 0;
+        pickedAnswers[3] = 0;
+        mapArray.forEach(function (pickedAnswer, i) {
+            pickedAnswers["" + pickedAnswer]++;
+        });
+        return {pickedAnswers};
+    };
+    mapReduceObject.query = {
+        homework: homework._id
+    };
+    mapReduceObject.finalize = function (keyExerciseIndex, reducedValue) {
+        return reducedValue;
+    };
+    SubmissionModel.mapReduce(mapReduceObject, function (err, statistics) {
+        if (err) {
+            res.status(500).json(err);
+        }
+        extendMapReduce(req, res, statistics, homework)
+    });
+}
+
+// See: http://mongoosejs.com/docs/api.html#mongoose_Mongoose-Model
+function mapReduceMembership() {
+    let mapReduceObject = {};
+    mapReduceObject.map = function () {
+        // each submission contains the exercises array which stores the picked answer for each exercise of the student
+        this.exercises.forEach(function (pickedAnswer, i) {
+            // key is index of the exercise in exercise array
+            // value is map of:
+            emit(i, pickedAnswer);
+        });
+    };
+    mapReduceObject.reduce = function (keyExerciseIndex, mapArray) {
+        let pickedAnswers = [];
+        pickedAnswers[0] = 0;
+        pickedAnswers[1] = 0;
+        pickedAnswers[2] = 0;
+        pickedAnswers[3] = 0;
+        mapArray.forEach(function (pickedAnswer, i) {
+            pickedAnswers["" + pickedAnswer]++;
+        });
+        return {pickedAnswers};
+    };
+    mapReduceObject.query = {
+        homework: homework._id
+    };
+    mapReduceObject.finalize = function (keyExerciseIndex, reducedValue) {
+        return reducedValue.value;
+    };
+    SubmissionModel.mapReduce(mapReduceObject, function (err, statistics) {
+        if (err) {
+            res.status(500).json(err);
+        }
+        extendMapReduce(req, res, statistics, homework)
+    });
+}
+
+function extendMapReduce(req, res, statistics, homework) {
+    SubmissionModel.find({homework: homework._id}).exec()
+        .then((submission) => {
+            const map = {};
+            map.count = submission.length;
+            map.submissions = submission;
+            map.homework = homework;
+            if(map.count === 0) res.status(200).json(map);
+            const exerciseStatistics = statistics.results;
+            exerciseStatistics.forEach(function (submittedExercise, i) {
+                exerciseStatistics["" + i] = {
+                    pickedAnswers: exerciseStatistics["" + i].value.pickedAnswers,
+                    answerPercentage: [
+                        exerciseStatistics["" + i].value.pickedAnswers[0] / map.count,
+                        exerciseStatistics["" + i].value.pickedAnswers[1] / map.count,
+                        exerciseStatistics["" + i].value.pickedAnswers[2] / map.count,
+                        exerciseStatistics["" + i].value.pickedAnswers[3] / map.count,
+                    ],
+                    rightAnswerPicked:
+                        exerciseStatistics["" + i].value.pickedAnswers["" + homework.exercises["" + i].rightSolution],
+                    rightAnswerPercentage:
+                    exerciseStatistics["" + i].value.pickedAnswers["" + homework.exercises["" + i].rightSolution] / map.count
+                };
+            });
+            map.exerciseStatistics = exerciseStatistics;
+            res.status(200).json(map);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(404).json({error: "Object not found"});
+        });
+}
 
 // body has to contain array of exercises, id student and id of the homework
 const create = (req, res) => {
@@ -38,7 +148,7 @@ const create = (req, res) => {
 };
 
 
-const findSubmissionOfStudentByHomework = (req, res) => {
+const findSubmissionOfUserByHomework = (req, res) => {
     const userId = req.userId;
     const homeworkId = req.params.id;
     SubmissionModel.find({homework: homeworkId, student: userId})
@@ -54,11 +164,11 @@ const findSubmissionOfStudentByHomework = (req, res) => {
         .catch(error => {
             res.status(500).json(error);
         })
-}
+};
 
 
 module.exports = {
-    findByHomework,
+    getStatisticsForHomework,
     create,
-    findSubmissionOfStudentByHomework
+    findSubmissionOfUserByHomework
 };
--- src/routes/submission.js
+++ src/routes/submission.js
@@ -6,12 +6,10 @@ const router = express.Router();
 const middleware = require('../middleware');
 const SubmissionController = require('../controllers/submission');
 
-//TODO add authentification for production
-//finds all submission by homework ID
-router.get('/:id', SubmissionController.findByHomework);
-
+//finds all submission by homework ID TODO middleware.checkAuthentication,
+router.get('/:id', SubmissionController.getStatisticsForHomework);
 //find submissions of one user for one homework, params :id is the id of homework, user id is delivered by aut
-router.get('/user/:id/',middleware.checkAuthentication, SubmissionController.findSubmissionOfStudentByHomework);
-router.post('/',middleware.checkAuthentication, SubmissionController.create);
+router.get('/user/:id/', middleware.checkAuthentication, SubmissionController.findSubmissionOfUserByHomework);
+router.post('/', middleware.checkAuthentication , SubmissionController.create);
 
 module.exports = router;
