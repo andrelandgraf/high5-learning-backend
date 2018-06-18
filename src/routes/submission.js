a01b059333be396d39f47b6fdf0722c476b61d17
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-06-18 20:15:32 +0200

    Check authentification for statistics. Was disabled for testing.

--- src/routes/submission.js
+++ src/routes/submission.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const middleware = require('../middleware');
 const SubmissionController = require('../controllers/submission');
 
-//finds all submission by homework ID TODO middleware.checkAuthentication,
-router.get('/:id', SubmissionController.getStatisticsForHomework);
+//finds all submission by homework ID
+router.get('/:id',  middleware.checkAuthentication, SubmissionController.getStatisticsForHomework);
 router.get('/ranking/:id', middleware.checkAuthentication, SubmissionController.getRankingOfSubmissions);
 //find submissions of one user for one homework, params :id is the id of homework, user id is delivered by aut
 router.get('/user/:id/', middleware.checkAuthentication, SubmissionController.findSubmissionOfUserByHomework);
